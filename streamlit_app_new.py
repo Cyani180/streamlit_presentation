611f712047de77aec03126ffb7cbcb8bbf26b198
Cyani180 <cyani180@protonmail.com>
2025-04-28 14:23:52 +0200

    Edit and new file

--- streamlit_app_new.py
+++ streamlit_app_new.py
@@ -1,4 +1,43 @@
+
+
+import pandas as pd
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+df = pd.read_csv("MovieReview.csv")
+st.dataframe(df.head(10))
+st.write(df.shape)
+st.dataframe(df.describe())
+
+df = df.drop('sentiment', axis=1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""import streamlit as st
 import random
 import pickle
 @st.cache_data
@@ -10,6 +49,4 @@ st.write(a)
 st.write(b)
 
 
-loaded_model = pickle.load(open("model", 'rb'))
-
-
+loaded_model = pickle.load(open("model", 'rb'))"""
